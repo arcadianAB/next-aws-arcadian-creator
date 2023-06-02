92debe75210fdf2c3957794de90016ea9f071997
lukas vidfar <dev@arcadian.se>
2023-06-02 16:31:09 +0400

    2023-06-02

--- src/comps/home/canvas/Staging.tsx
+++ src/comps/home/canvas/Staging.tsx
@@ -4,7 +4,11 @@ import React from "react";
 const Staging = () => {
   return (
     <>
-      <Environment preset="warehouse" />
+      <directionalLight
+        intensity={0.5}
+        color={"#f5f5f5"}
+        position={[5, 5, 5]}
+      />
     </>
   );
 };
